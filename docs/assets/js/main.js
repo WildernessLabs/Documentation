a7701a8204f12473fa1efd6fa9410843e7f2b21e
Stephen Karpeles <skarpeles@bluechipww.com>
2018-02-06 13:24:58 -0600

    Get interior nav to be functional

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,5 +1,26 @@
-// Mobile menu toggle
+// Submenu toggle
 window.onload = function(){
+  const elAside = document.querySelector('.mobile-toggle--aside');
+  const elAsideAlt = document.querySelector('.mobile-toggle--aside-alt');
+  const navAside = document.querySelector('.interior-aside');
+
+  if(elAside) {
+    elAside.addEventListener('click', (e) => {
+      e.preventDefault();
+      navAside.classList.toggle('is-open');
+      elAside.classList.toggle('has-open-nav');
+    });
+  }
+
+  if (elAsideAlt) {
+    elAsideAlt.addEventListener('click', (e) => {
+      e.preventDefault();
+      navAside.classList.toggle('is-open');
+      elAside.classList.toggle('has-open-nav');
+    });
+  }
+
+// Mobile menu toggle
   const el = document.querySelector('.mobile-toggle');
   const nav = document.querySelector('.main-nav');
   const header = document.querySelector('.header');
@@ -11,23 +32,3 @@ window.onload = function(){
     header.classList.toggle('has-open-nav');
   });
 };
-
-
-// Submenu toggle
-// window.onload = function(){
-//   const elAside = document.querySelector('.mobile-toggle--aside');
-//   const elAsideAlt = document.querySelector('.mobile-toggle--aside-alt');
-//   const navAside = document.querySelector('.interior-aside');
-// 
-//   elAside.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     navAside.classList.toggle('is-open');
-//     elAside.classList.toggle('has-open-nav');
-//   });
-// 
-//   elAsideAlt.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     navAside.classList.toggle('is-open');
-//     elAside.classList.toggle('has-open-nav');
-//   });
-// };
