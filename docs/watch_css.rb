2afb0ef607d07e9601f2d9f33ae37f38585daf9b
Brandon Bilfield <brandx105@gmail.com>
2019-08-10 09:07:46 -0700

    Updating File Structure + CSS watch script

---
+++ docs/watch_css.rb
@@ -0,0 +1,52 @@
+require 'filewatcher'
+require 'fileutils'
+
+path_from_dist = '/Users/bbilfield/Library/Mobile Documents/com~apple~CloudDocs/Work/personal/wilderness/code/web.full/src/WildernessLabs.Web/wwwroot/dist/'
+
+
+# CSS Watcher
+
+path_from_css = path_from_dist + 'css'
+path_to_dist_css = './assets/dist/css'
+
+puts "Watching -- " + path_from_css
+
+Filewatcher.new([path_from_css]).watch do |filename, event|
+
+  # specify files which should not be copied
+  dont_copy = []
+
+  puts "Copying files from WildernessLabs CSS to Documentation site --------------- "
+
+  from_dir = path_from_css
+  to_dir = path_to_dist_css
+
+  contains = Dir.new(from_dir).entries
+
+  def copy_with_path(src, dst)
+    FileUtils.mkdir_p(File.dirname(dst))
+    FileUtils.cp(src, dst)
+  end
+
+  puts "Emptying directory target --------------- "
+  FileUtils.rm_rf(to_dir)
+
+  puts "Copying new files --------------- "
+  FileUtils.cp_r(from_dir, to_dir);
+  puts "Copying CSS Complete --------------- "
+
+  puts "Updating file paths in CSS --------------- "
+  file_names = [path_to_dist_css+'/main.css']
+
+  file_names.each do |file_name|
+    text = File.read(file_name)
+    new_contents = text.gsub("../../assets/", "/assets/dist/assets/")
+
+    # To write changes to the file, use:
+    File.open(file_name, "w") {|file| file.puts new_contents }
+  end
+
+  puts "Path Updating Complete --------------- "
+  puts "Watching " + path_from_css
+  puts "-"
+end
