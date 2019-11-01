ce27435372de73ea877295b163d694d30f620a9f
Brandon Bilfield <bbilfield@liveareacx.com>
2019-11-01 09:30:17 -0700

    Updating Jekyll Docs 'dist' location

--- docs/watch.rb
+++ docs/watch.rb
@@ -5,53 +5,45 @@ require 'json'
 file = File.read('watch_config.json')
 data_hash = JSON.parse(file)
 
-# Path to /dist/ directory in the Wildernesslabs Repo
-path_from_dist = data_hash['path_from_dist'];
-
 # CSS Watcher
-path_from_css = path_from_dist + 'css'
-path_to_dist_css = './assets/dist/css'
+path_from_dist = data_hash['path_from_dist']
+path_to_dist = './dist'
 
 if path_from_dist.eql? "<path to wwwroot dist>"
   raise ArgumentError, 'You need to configure your path_from_dist variable, point this to the location of your Wildernesslabs wwwroot dist directory and try running this task again.'
 else
-  puts "Watching -- " + path_from_css
+  puts "Watching -- " + path_from_dist
 
-  Filewatcher.new([path_from_css]).watch do |filename, event|
+  Filewatcher.new([path_from_dist]).watch do |filename, event|
 
     # specify files which should not be copied
     puts "Copying files from WildernessLabs CSS to Documentation site --------------- "
 
-    from_dir = path_from_css
-    to_dir = path_to_dist_css
+    from_dir = path_from_dist
+    to_dir = path_to_dist
 
     contains = Dir.new(from_dir).entries
-
-    def copy_with_path(src, dst)
-      FileUtils.mkdir_p(File.dirname(dst))
-      FileUtils.cp(src, dst)
-    end
 
     puts "Emptying directory target --------------- "
     FileUtils.rm_rf(to_dir)
 
-    puts "Copying new files --------------- "
+    puts "Copying new DIST files --------------- "
     FileUtils.cp_r(from_dir, to_dir);
-    puts "Copying CSS Complete --------------- "
+    puts "Copying DIST Complete --------------- "
 
     puts "Updating file paths in CSS --------------- "
-    file_names = [path_to_dist_css+'/main.css']
+    file_names = [path_to_dist +'/css/main.css']
 
     file_names.each do |file_name|
       text = File.read(file_name)
-      new_contents = text.gsub("../../assets/", "/assets/dist/assets/")
+      new_contents = text.gsub("../../assets/", "/dist/assets/")
 
       # To write changes to the file, use:
       File.open(file_name, "w") {|file| file.puts new_contents }
     end
 
     puts "Path Updating Complete --------------- "
-    puts "Watching " + path_from_css
+    puts "Watching " + path_from_dist
     puts "-"
   end
 end
