d3a00a4c92b947f7756cc203caaf4e35a9129b95
Adam Dunn <adunndevster@gmail.com>
2023-12-08 14:49:16 -0700

    removed docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/adam/_/Documentation/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/adam/_/Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/adam/_/Documentation/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/adam/_/Documentation/src/css/custom.scss'),
-];
