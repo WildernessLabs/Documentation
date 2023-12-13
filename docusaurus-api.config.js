2dee78546c3258dd84dbc6ecb5bd01118328c2d3
Adam Dunn <adunndevster@gmail.com>
2023-12-13 16:02:56 -0700

    search integration

--- docusaurus-api.config.js
+++ docusaurus-api.config.js
@@ -212,7 +212,36 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         respectPrefersColorScheme: true,
-      }
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'A4D5IOMP59',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'b72b27b61280e89266704664307c36e5',
+  
+        indexName: 'developer-usaurus-wildernesslabs',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 };
 
