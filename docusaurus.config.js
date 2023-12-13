0af79d53c56fd86d3277981ae09121cc93490866
Adam Dunn <adunndevster@gmail.com>
2023-12-12 17:33:04 -0700

    build api

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/WildernessLabs/DocsDocusaurus/tree/main/',
-          routeBasePath: '/'
+          // routeBasePath: '/'
         },
         blog: {
           showReadingTime: true,
