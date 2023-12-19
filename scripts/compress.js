88c6db5b9e0d713938023aec9576936dc64d3af1
Adam Dunn <adunndevster@gmail.com>
2023-12-19 16:33:50 -0700

    added compress script

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,6 +1,7 @@
 const sharp = require("sharp");
 const glob = require("glob");
-const fs = require("fs");
+const fs = require('fs');
+
 
 const targetFolder = process.argv[2] || "."; // You can specify the folder as a command-line argument
 
@@ -11,21 +12,37 @@ const compressImage = (filePath) => {
   if (filePath.endsWith("png")) {
     sharp(filePath)
       .png({ quality: 80 })
-      .toBuffer(function (err, buffer) {
-        fs.writeFile(filePath, buffer, function (e) {});
-      })
+      .toFile(filePath + '__tmp')
       .then(() => console.log(`Compressed: ${outputPath}`))
       .catch((err) => console.error(`Error compressing ${filePath}:`, err));
-  } else if (filePath.endsWith("jpg") || filePath.endsWith("jpeg")) {
+  } else if (filePath.endsWith("jpg") || filePath.endsWith("jpeg"))
+  {
     sharp(filePath)
       .jpeg({ mozjpeg: true, quality: 80 })
-      .toBuffer(function (err, buffer) {
-        fs.writeFile(filePath, buffer, function (e) {});
-      })
+      .toFile(filePath + '__tmp')
       .then(() => console.log(`Compressed: ${outputPath}`))
       .catch((err) => console.error(`Error compressing ${filePath}:`, err));
   }
 };
+
+const renameImage = (filePath) => {
+    console.log(`Attempting to rename: ${filePath}`);
+
+    const tmpPath = filePath + "__tmp";
+
+    try {
+        fs.unlinkSync(filePath);
+        console.log(`File deleted: ${filePath}`);
+    } catch (err) {
+        console.error('Error deleting file:', err);
+    }
+    
+    try {
+        fs.renameSync(tmpPath, filePath);
+    } catch (err) {
+        console.error('Error renaming file:', err);
+    }
+}
 
 // Glob pattern to find images (jpg and png)
 const pattern = `${targetFolder}/**/*.+(jpg|jpeg|png)`;
@@ -38,3 +55,4 @@ images = glob.globSync(pattern, (err, files) => {
 });
 
 images.forEach(compressImage);
+images.forEach(renameImage);
