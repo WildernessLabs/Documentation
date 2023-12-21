5969ebce75f53aca42aa1bf6248415bce1965312
Adam Dunn <adunndevster@gmail.com>
2023-12-21 10:56:07 -0700

    image resizing if greater than 1000px in width

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,30 +1,42 @@
 const sharp = require("sharp");
 const glob = require("glob");
-const fs = require('fs');
-
+const fs = require("fs");
 
 const targetFolder = process.argv[2] || "."; // You can specify the folder as a command-line argument
 
 // Function to compress images
 const compressImage = (filePath) => {
-//   console.log(`Attempting to compress: ${filePath}`);
-  ("");
+  //   console.log(`Attempting to compress: ${filePath}`);
+  let image = sharp(filePath);
   if (filePath.endsWith("png")) {
-    sharp(filePath)
+    image
+      .metadata()
+      .then((metadata) => {
+        if (metadata.width > 1000) {
+          return image.resize(1000).png().toBuffer();
+        }
+        return image;
+      })
       .png({ quality: 80 })
       .toFile(filePath + "__tmp")
       .then(() => {
-        console.log(`Compressed: ${filePath}`)
+        console.log(`Compressed: ${filePath}`);
         renameImage(filePath);
       })
       .catch((err) => console.error(`Error compressing ${filePath}:`, err));
-  } else if (filePath.endsWith("jpg") || filePath.endsWith("jpeg"))
-  {
-    sharp(filePath)
+  } else if (filePath.endsWith("jpg") || filePath.endsWith("jpeg")) {
+    image
+      .metadata()
+      .then((metadata) => {
+        if (metadata.width > 1000) {
+          return image.resize(1000).jpeg().toBuffer();
+        }
+        return image;
+      })
       .jpeg({ mozjpeg: true, quality: 80 })
       .toFile(filePath + "__tmp")
       .then(() => {
-        console.log(`Compressed: ${filePath}`)
+        console.log(`Compressed: ${filePath}`);
         renameImage(filePath);
       })
       .catch((err) => console.error(`Error compressing ${filePath}:`, err));
@@ -32,24 +44,23 @@ const compressImage = (filePath) => {
 };
 
 const renameImage = (filePath) => {
-    console.log(`Attempting to rename: ${filePath}`);
+  console.log(`Attempting to rename: ${filePath}`);
 
-    const tmpPath = filePath + "__tmp";
+  const tmpPath = filePath + "__tmp";
 
-    try {
-        fs.unlinkSync(filePath);
-        console.log(`File deleted: ${filePath}`);
-    } catch (err) {
-        console.error('Error deleting file:', err);
-    }
-    
-    try {
-        fs.renameSync(tmpPath, filePath);
-    } catch (err) {
-        console.error('Error renaming file:', err);
-    }
+  try {
+    fs.unlinkSync(filePath);
+    console.log(`File deleted: ${filePath}`);
+  } catch (err) {
+    console.error("Error deleting file:", err);
+  }
 
-}
+  try {
+    fs.renameSync(tmpPath, filePath);
+  } catch (err) {
+    console.error("Error renaming file:", err);
+  }
+};
 
 // Glob pattern to find images (jpg and png)
 const pattern = `${targetFolder}/**/*.+(jpg|jpeg|png)`;
