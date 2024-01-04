c8fa634d73be90eccdcde2355a938bc795bf9e9f
Adam Dunn <adunndevster@gmail.com>
2024-01-04 11:56:08 -0700

    updated copy and folder struct

--- scripts/copy-api.js
+++ scripts/copy-api.js
@@ -2,21 +2,16 @@ const fse = require('fs-extra');
 const path = require('path');
 
 // Define source and destination directories
-const source1 = path.join('.', 'docfx', 'api-override');
-const source2 = path.join('.', 'api');
+const source = path.join('.', 'api');
 const destination = path.join('.', 'docs', 'api');
 
 
 // Function to copy directories
 async function copyDirectories() {
     try {
-        // Copying contents from "./docfx/api-override/" to "./docs/api/"
-        await fse.copy(source1, destination);
-        console.log(`Copied from ${source1} to ${destination}`);
-
         // Copying contents from "./api/" to "./docs/api/"
-        await fse.copy(source2, destination);
-        console.log(`Copied from ${source2} to ${destination}`);
+        await fse.copy(source, destination);
+        console.log(`Copied from ${source} to ${destination}`);
 
         console.log('Copy operations completed successfully!');
     } catch (err) {
