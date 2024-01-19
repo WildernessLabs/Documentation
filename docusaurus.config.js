e38adeace641a5a913a85cf269edc795fdae62c2
Adam Dunn <adunndevster@gmail.com>
2024-01-19 09:38:31 -0700

    fixed hreflang

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   plugins: ['docusaurus-plugin-sass'],
 
   // Set the production url of your site here
-  url: 'https://wildernesslabs.github.io',
+  url: 'https://developer-docusaurus.wildernesslabs.co/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
