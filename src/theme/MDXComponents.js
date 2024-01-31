8acd566a17e518e77ab5daf0b261ba148cb73723
Adam Dunn <adunndevster@gmail.com>
2024-01-31 09:23:34 -0700

    admonitions

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,9 +1,11 @@
 // src/theme/MDXComponents.js
+import Admonition from '@theme/Admonition';
 import GettingStarted from '../components/GettingStarted';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
 export default {
+    Admonition,
     GettingStarted,
     Tabs,
     TabItem
