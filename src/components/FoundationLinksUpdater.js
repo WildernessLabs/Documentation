a8e03fa5c4ce03bae09f5a3e08a50105b317e192
Adam Dunn <adunndevster@gmail.com>
2024-02-29 20:55:42 -0700

    trying on page load

--- src/components/FoundationLinksUpdater.js
+++ src/components/FoundationLinksUpdater.js
@@ -1,6 +1,37 @@
 import React, { useEffect } from "react";
 import { useLocation } from "@docusaurus/router";
 
+// let count = 0;
+function updateLinks() {
+  if (!location.pathname.includes("/docs/api/Meadow.Foundation")) {
+    return null;
+  }
+  const links = document.querySelectorAll(".menu__link, .toc-item a");
+  // if (!links?.length && count < 30) {
+  //   count++;
+  //   setTimeout(() => updateLinks(), 100);
+  // }
+  links.forEach((link) => {
+    if (link.href.includes("/docs/api/Meadow.Foundation.mikroBUS/")) {
+      link.innerText = link.innerText.replace(
+        "Meadow.Foundation.mikroBUS.",
+        ""
+      );
+    } else if (
+      link.href.includes("/docs/api/Meadow.Foundation.FeatherWings/")
+    ) {
+      link.innerText = link.innerText.replace(
+        "Meadow.Foundation.FeatherWings.",
+        ""
+      );
+    } else if (link.href.includes("/docs/api/Meadow.Foundation.Grove/")) {
+      link.innerText = link.innerText.replace("Meadow.Foundation.Grove.", "");
+    } else if (link.href.includes("/docs/api/Meadow.Foundation/")) {
+      link.innerText = link.innerText.replace("Meadow.Foundation.", "");
+    }
+  });
+}
+
 const FoundationLinksUpdater = () => {
   const location = useLocation();
 
@@ -9,46 +40,6 @@ const FoundationLinksUpdater = () => {
   }, []); // This effect runs once on component mount
 
   useEffect(() => {
-    if(!location.pathname.includes("/docs/api/Meadow.Foundation"))
-    {
-      return null;
-    }
-
-    let count = 0;
-    function updateLinks() {
-      const links = document.querySelectorAll(".menu__link, .toc-item a");
-      if(!links?.length && count<30)
-      {
-        count++;
-        setTimeout(() => updateLinks(), 100);
-      }
-      links.forEach((link) => {
-        if (link.href.includes("/docs/api/Meadow.Foundation.mikroBUS/")) {
-          link.innerText = link.innerText.replace(
-            "Meadow.Foundation.mikroBUS.",
-            ""
-          );
-        } else if (
-          link.href.includes("/docs/api/Meadow.Foundation.FeatherWings/")
-        ) {
-          link.innerText = link.innerText.replace(
-            "Meadow.Foundation.FeatherWings.",
-            ""
-          );
-        } else if (link.href.includes("/docs/api/Meadow.Foundation.Grove/")) {
-          link.innerText = link.innerText.replace(
-            "Meadow.Foundation.Grove.",
-            ""
-          );
-        } else if (link.href.includes("/docs/api/Meadow.Foundation/")) {
-          link.innerText = link.innerText.replace(
-            "Meadow.Foundation.",
-            ""
-          );
-        }
-      });
-    }
-
     updateLinks();
   }, [location.pathname]); // This effect runs when the pathname changes
 
