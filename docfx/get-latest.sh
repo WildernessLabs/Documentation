5cd66a34037c28f76d5cbcd8acc38a33531e7e63
Adam Patridge <patridge@gmail.com>
2025-04-26 19:27:45 -0600

    Update get-latest script with newer repos

--- docfx/get-latest.sh
+++ docfx/get-latest.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 set -x
 
-cd ../../Meadow.Core
+# Up to repo root
+currentDir=$(pwd)
+
+cd ..
+cd ../Meadow.Units
+git pull
+cd ../Meadow.Logging
+git pull
+cd ../Meadow.Contracts
+git pull
+cd ../Meadow.Modbus
 git pull
 cd ../Meadow.Foundation
+git pull
+cd ../Meadow.Core
+git pull
+cd ../MQTTnet
+git pull
+cd ../Meadow
+git pull
+cd ../Meadow.Foundation.CompositeDevices
 git pull
 cd ../Meadow.Foundation.Grove
 git pull
 cd ../Meadow.Foundation.FeatherWings
 git pull
-cd ../Meadow.Docs/docfx
+cd ../Meadow.Foundation.mikroBUS
+git pull
+cd ../Meadow.Foundation.MBus
+git pull
+
+cd "$currentDir"
